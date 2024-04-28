69b6e65390b2d3b51cbc32b864e4920745c9af3c
AndeersonDEV <andeerson_sps@hotmail.com>
2024-04-28 13:14:46 -0300

    alterando o limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumerioAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto){
 }
 
 function exibirMensgameInicial(){
-exibirTextoNaTela('h1','Ola Denis Volei')
+exibirTextoNaTela('h1','Jogo do número secreto')
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
 }
 
